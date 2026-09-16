53d3c3fb8211c517a508ea1358a9330d46f34e7c
Martin Knöbelreiter <martin.knoeb@gmail.com>
2026-09-16 11:53:27 +0200

    initialize

---
+++ code/src/main/java/pocket/moneychecker/MoneycheckerApplication.java
@@ -0,0 +1,13 @@
+package pocket.moneychecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneycheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneycheckerApplication.class, args);
+	}
+
+}
---
+++ code/src/test/java/pocket/moneychecker/MoneycheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package pocket.moneychecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneycheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
